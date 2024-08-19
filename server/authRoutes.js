422c3018b8ecc6fab4bbcd7a025717e01d2a8abc
Mohammed-Adel56 <168343516+Mohammed-Adel56@users.noreply.github.com>
2024-08-19 23:59:06 +0300

    change root

--- server/authRoutes.js
+++ server/authRoutes.js
@@ -150,4 +150,61 @@ router.post("/resetPassword", async (req, res) => {
   }
 });
 
+router.get("/get", (req, res) => {
+  const { token } = req.cookies;
+  if (token) {
+    jwt.verify(token, "kjsaiowq458", {}, async (err, user) => {
+      if (err) {
+        res.json({ error: err });
+      }
+      const query = await User.findOne({ _id: user.id }).select("todos");
+      res.json(query.todos);
+    });
+  }
+});
+router.delete("/delete/:id", (req, res) => {
+  const { id } = req.params;
+  const { token } = req.cookies;
+
+  if (token) {
+    jwt.verify(token, "kjsaiowq458", {}, async (err, user) => {
+      if (err) {
+        res.json({ error: err });
+      }
+
+      await User.findById(user.id)
+        .then(async (user) => {
+          if (user && user.todos.length > id) {
+            user.todos.splice(id, 1);
+
+            await user.save();
+          } else {
+            res.json({ error: "Index out of bounds or user not found" });
+          }
+        })
+        .catch((err) => console.log(err));
+      const query = await User.findOne({ _id: user.id }).select("todos");
+      res.json(query.todos);
+    });
+  }
+});
+router.post("/add", (req, res) => {
+  const todo = req.body.todo;
+  const { token } = req.cookies;
+  if (token) {
+    jwt.verify(token, "kjsaiowq458", {}, async (err, user) => {
+      if (err) {
+        res.json({ error: err });
+      }
+
+      const newUp = await User.updateOne(
+        { _id: user.id },
+        { $push: { todos: todo } }
+      );
+      const query = await User.findOne({ _id: user.id }).select("todos");
+      res.json(query.todos);
+    });
+  }
+});
+
 module.exports = router;
--- server/index.js
+++ server/index.js
@@ -28,7 +28,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/", require("./authRoutes"));
-app.use("/", require("./todoRoutes"));
+// app.use("/", require("./todoRoutes"));
 app.listen(port, () =>
   console.log(`Server is running on port ${port} link http://localhost:${port}`)
 );
