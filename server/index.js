8c4b48f37f23a3c65c34099d8da8b5353de4748a
Mohammed-Adel56 <168343516+Mohammed-Adel56@users.noreply.github.com>
2024-08-20 00:49:24 +0300

    up

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 //database connection
 mongoose
   .connect(
